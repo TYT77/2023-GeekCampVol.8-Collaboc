d131f2a4e73d653e6e4c02b5fc3428f4e978ee65
yukke23 <ec98721zzz@gmail.com>
2023-05-17 20:39:02 +0900

    update nav bar

--- app/vue-md-store/src/components/Navigation.vue
+++ app/vue-md-store/src/components/Navigation.vue
@@ -1,9 +1,48 @@
 <template>
-    <nav>
-        <router-link to="/">Home | </router-link>
-        <router-link to="/feed" v-if="isLoggedIn">Feed | </router-link>
-        <router-link to="/sign-in" v-else="isLoggedIn">Login</router-link>
-        <a @click="handleSignOut" v-if="isLoggedIn">Sign Out</a>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        
+        <!-- スマホで表示されるモノ -->
+        <div class="navbar-brand">
+            
+            <!-- ロゴ -->
+            <router-link class="navbar-item" to="/">
+                <img src="/vite.svg" width="112" height="28">
+            </router-link>
+            
+            <!-- ハンバーガーメニュー -->
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+
+        </div>
+
+        <!-- PCだとそのまま表示されるモノ -->
+        <div id="navbarBasicExample" class="navbar-menu">
+            
+            <div class="navbar-start">
+
+                <router-link to="/" class="navbar-item">Home</router-link>
+                <router-link to="/todo" v-if="isLoggedIn" class="navbar-item">Todo</router-link>
+
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <router-link to="/sign-in" v-if="!isLoggedIn" class="button is-primary">    
+                            <strong>Log in</strong>
+                        </router-link>
+                        <a @click="handleSignOut" v-if="isLoggedIn" class="button is-light">
+                            <strong>Sign Out</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </nav>
 </template>
 
@@ -32,4 +71,28 @@ const handleSignOut = () => {
     })
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
+        });
+    });
+});
+
 </script>
+
+<style scoped>
+    @import 'bulma/css/bulma.css'
+</style>
