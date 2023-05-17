2b8a3a71965d2f1e4e6af7979769c4a73bc5779e
yukke23 <ec98721zzz@gmail.com>
2023-05-17 21:22:42 +0900

    Hide where firebase settings are loaded

--- app/vue-md-store/src/main.js
+++ app/vue-md-store/src/main.js
@@ -2,21 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { initializeApp} from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC4mLSC3hCAhIyVR43KSEGREJwRWFA3WCg",
-  authDomain: "vue-md-store.firebaseapp.com",
-  projectId: "vue-md-store",
-  storageBucket: "vue-md-store.appspot.com",
-  messagingSenderId: "326602806464",
-  appId: "1:326602806464:web:24cf6db94246670a1e8925",
-  measurementId: "G-GG21GPLL7G"
-};
-
-const analytics = getAnalytics(initializeApp(firebaseConfig));
-
 const app = createApp(App)
   .use(router)
   .mount('#app')
